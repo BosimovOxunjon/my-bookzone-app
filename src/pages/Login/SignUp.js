46324b3bb3abec577bfb32461ac86dbec552e243
Bosimov Oxunjon <bosimovoxunjon@gmail.com>
2022-11-28 17:48:47 +0500

    some bugs have been fixed

--- src/pages/Login/SignUp.js
+++ src/pages/Login/SignUp.js
@@ -7,71 +7,69 @@ import { StyledSignUp } from "../../style/pages/signUp";
 const SignUp = () => {
   return (
     <StyledSignUp>
-      <div className="container">
-        <div className="row">
-          <div className="img">
-            <div className="img_wrapper">
-              <img src={BgImg} className="img_item" alt="background-img" />
-              <img src={MainImg} className="img_item" alt="background-img" />
-            </div>
+      <div className="row">
+        <div className="img">
+          <div className="img_wrapper">
+            <img src={BgImg} className="img_item" alt="background-img" />
+            <img src={MainImg} className="img_item" alt="background-img" />
           </div>
-          <form className="form_wrapper">
-            <h2 className="title">Sign Up</h2>
-            <p className="text">
-              Already have an account.
-              {/* <Link to={`/`}>Sign In</Link> */}
-            </p>
-            <input
-              className="form_input"
-              type="text"
-              name="Name"
-              placeholder="First name"
-              required
-            />{" "}
-            <br />
-            <input
-              className="form_input"
-              type="text"
-              name="Last name"
-              placeholder="Last name"
-              required
-            />{" "}
-            <br />
-            <input
-              className="form_input"
-              type="tel"
-              name="phone"
-              placeholder="Phone"
-              defaultValue="+998"
-              pattern="[+]{1}[0-9]{3}[0-9]{2}[0-9]{2}"
-              maxLength="13"
-              required
-            />
-            <br />
-            <input
-              className="form_input"
-              type="mail"
-              name="mail"
-              placeholder="Email"
-              required
-            />
-            <br />
-            <input
-              className="form_input"
-              type="password"
-              name="password"
-              placeholder="Password"
-              required
-            />{" "}
-            <br />
-            <input
-              className="form_submit"
-              type="submit"
-              name="button"
-              value="Next step"
-            />
-          </form>
         </div>
+        <form className="form_wrapper">
+          <h2 className="title">Sign Up</h2>
+          <p className="text">
+            Already have an account.
+            {/* <Link to={`/`}>Sign In</Link> */}
+          </p>
+          <input
+            className="form_input"
+            type="text"
+            name="Name"
+            placeholder="First name"
+            required
+          />{" "}
+          <br />
+          <input
+            className="form_input"
+            type="text"
+            name="Last name"
+            placeholder="Last name"
+            required
+          />{" "}
+          <br />
+          <input
+            className="form_input"
+            type="tel"
+            name="phone"
+            placeholder="Phone"
+            defaultValue="+998"
+            pattern="[+]{1}[0-9]{3}[0-9]{2}[0-9]{2}"
+            maxLength="13"
+            required
+          />
+          <br />
+          <input
+            className="form_input"
+            type="mail"
+            name="mail"
+            placeholder="Email"
+            required
+          />
+          <br />
+          <input
+            className="form_input"
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />{" "}
+          <br />
+          <input
+            className="form_submit"
+            type="submit"
+            name="button"
+            value="Next step"
+          />
+        </form>
       </div>
     </StyledSignUp>
   );
--- src/style/pages/signUp.js
+++ src/style/pages/signUp.js
@@ -2,27 +2,29 @@ import styled from "styled-components";
 import "bootstrap";
 
 const StyledSignUp = styled.section`
+  .row {
+    display: flex;
+    align-items: center;
+  }
   .img {
     width: 50%;
+    margin-left: 0;
   }
   .img_wrapper {
     display: flex;
     positon: relative;
-    justify-content: center;
-    .img_item {
+    justify-content: flex-start;
+    align-items: center;
+    .img_item:last-child {
       position: absolute;
-    }
-    .img_item:first-child {
-      left: 0;
-      top: 0;
-      bottom: 0;
     }
   }
   .form_wrapper {
-    display: grid;
-    width: 50%;
-    margin-left: auto;
-    justify-content: space-around;
+    display: flex;
+    flex-direction: column;
+    width: 70%;
+    // margin-left: auto;
+    // justify-content: space-around;
     .title {
       font-family: "Arial Black";
       font-style: normal;
@@ -40,14 +42,15 @@ const StyledSignUp = styled.section`
     }
     .form_input {
       border: 1px solid #b4b4bb;
+      width: 50%;
       border-radius: 10px;
       padding: 15px 0 15px 30px;
       font-size: 14px;
-      min-width: 300px;
     }
     .form_submit {
       font-style: normal;
       font-weight: 500;
+      width: 50%;
       font-size: 16px;
       padding: 12px 0;
       color: white;
@@ -58,9 +61,24 @@ const StyledSignUp = styled.section`
     }
   }
   @media screen and (max-width: 769px) {
+    .row {
+      display: block;
+      margin: 0 auto;
+    }
     .form_wrapper,
     .img {
       width: 100%;
+      align-items: center;
+    }
+    .img_item:first-child {
+      left: inherit;
+    }
+  }
+  @media screen and (max-width: 576px) {
+    .form_wrapper {
+      .form_input {
+        width: 95%;
+      }
     }
   }
 `;
