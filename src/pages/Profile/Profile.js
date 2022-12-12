a5d7a8623797b1d36cce2a0f748ee0f2206ad764
Bosimov Oxunjon <bosimovoxunjon@gmail.com>
2022-12-12 17:44:09 +0500

    some bugs have been fixed

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,38 +1,119 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { AiFillStar, AiFillAppstore } from "react-icons/ai";
+import { MdRefresh } from "react-icons/md";
 import ProfilePhoto from "../../assets/images/profile/man.jpeg";
+import { Layout, Col, Row, Space, Typography, Divider, Container } from "antd";
+import Card from "../../components/Card";
+import HomeImg from "../../assets/images/home/home.png";
 import { StyledProfile } from "../../style/pages/profile";
+import keys from "../../configs";
 import Header from "../Header/Header";
 
 const Profile = () => {
+  const [books, setBooks] = useState({ payload: { docs: [{}] } });
+  const fetchBooks = async () => {
+    const urlBooks = `${keys.BACKEND_API}/api/books`;
+    const { data } = await axios.get(urlBooks);
+    return data;
+  };
+  useEffect(() => {
+    fetchBooks().then((data) => {
+      setBooks(data);
+    });
+  }, []);
+  console.log(books);
   return (
     <>
-      <Header />
-      <StyledProfile>
-        <div className="container">
-          <div className="profile">
-            <div className="profile_img-wrapper">
-              <div className="profile_img">
-                <img src={ProfilePhoto} alt="profile_photo" />
+      <Layout>
+        <Header />
+        <StyledProfile>
+          <div className="container">
+            <div className="profile">
+              <div className="profile_img-wrapper">
+                <div className="profile_img">
+                  <img src={ProfilePhoto} alt="profile_photo" />
+                  <span className="profile_img-icon">
+                    <AiFillStar className="profile_img-icon-item" />
+                  </span>
+                </div>
+                <p>Oltin kitobxon</p>
+                <p>186 ta kitob o'qigan</p>
               </div>
-              <p>Oltin kitobxon</p>
-              <p>186 ta kitob o'qigan</p>
+              <div className="profile_info">
+                <h2 className="profile_title">farrux abdullayev</h2>
+                <p className="profile_text">
+                  <span>Tavallud :</span> February
+                </p>
+                <p className="profile_text">
+                  {" "}
+                  <span>Manzili : </span> Jizzax
+                </p>
+                <p className="profile_text">
+                  <span>Bio :</span> Graphic designer and Developer
+                </p>
+              </div>
             </div>
-            <div className="profile_info">
-              <h2 className="profile_title">farrux abdullayev</h2>
-              <p className="profile_text">
-                <span>Tavallud :</span> February
-              </p>
-              <p className="profile_text">
-                {" "}
-                <span>Manzili : </span> Jizzax
-              </p>
-              <p className="profile_text">
-                <span>Bio :</span> Graphic designer and Developer
-              </p>
-            </div>
+            <Row>
+              <Col sm={6}>
+                <div className="books_reading-wrapper">
+                  <p className="books_reading-title">
+                    Hozir o'qilmoqda...{" "}
+                    <span className="books_reading-icon">
+                      <AiFillAppstore />
+                    </span>
+                  </p>
+                  <div className="books_reading-card">
+                    <div className="books_reading-card-img">
+                      <img src={ProfilePhoto} alt="ikki eshik orasi" />
+                    </div>
+                    <div className="books_reading-text">
+                      <p>Ikki eshik orasi</p>
+                      <span></span>
+                    </div>
+                    <div className="books_reading-percentage">
+                      <p>96%</p>
+                      <button>
+                        <span>
+                          Yangilash
+                          <MdRefresh className="books_reading-percentage-icon" />
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+              <Col sm={12}>
+                <Space split={<Divider type="vertical" />}>
+                  <Typography.Link>O'qilganlar</Typography.Link>
+                  <Typography.Link>O'qishni xohlayman</Typography.Link>
+                  <Typography.Link>O'qilmoqda</Typography.Link>
+                  <Typography.Link>Adiblar</Typography.Link>
+                </Space>
+                <Row style={{ gap: "15px" }}>
+                  {books.payload.docs.map((item) => {
+                    return (
+                      <>
+                        <Col>
+                          <Card
+                            key={item?._id}
+                            id={item?._id}
+                            img={item?.imageLink || HomeImg}
+                            title={item?.title}
+                            // text={item?.author.firstName}
+                            view={item?.views}
+                          />
+                        </Col>
+                      </>
+                    );
+                  })}
+                  <Col></Col>
+                </Row>
+              </Col>
+            </Row>
           </div>
-        </div>
-      </StyledProfile>
+        </StyledProfile>
+      </Layout>
     </>
   );
 };
--- src/style/pages/profile.js
+++ src/style/pages/profile.js
@@ -16,21 +16,56 @@ const StyledProfile = styled.section`
       #2a2828 100%
     );
     box-shadow: 0px 4px 73px rgba(0, 0, 0, 0.25);
-    .profile_img {
-      display: inline-block;
-      width: 180px;
-      height: 180px;
-      border: 2px solid #ffffff;
-      border-radius: 50%;
-      overflow: hidden;
-      img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        object-position: left;
+    .profile_img-wrapper {
+      position: relative;
+      .profile_img {
+        display: inline-block;
+        width: 180px;
+        height: 180px;
+        border: 2px solid #ffffff;
+        border-radius: 50%;
+        overflow: hidden;
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          object-position: left;
+        }
+        .profile_img-icon {
+          position: absolute;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          right: 25px;
+          top: 48%;
+          width: 40px;
+          height: 40px;
+          border-radius: 50%;
+          font-size: 25px;
+          border: 2px solid #c9ac8c;
+          color: #c9ac8c;
+          background: black;
+        }
+      }
+      .profile_img-title {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 22px;
+        margin-bottom: 6px;
+        line-height: 144.4%;
+        color: #c9ac8c;
+      }
+      .profile_img-text {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 22px;
+        margin-top: 0;
+        line-height: 144.4%;
+        color: #ffffff;
       }
     }
     .profile_info {
+      padding: 3%;
       .profile_title {
         font-style: normal;
         font-weight: 400;
@@ -46,6 +81,84 @@ const StyledProfile = styled.section`
         span {
           line-height: 144.4%;
           color: #ffffff;
+        }
+      }
+    }
+  }
+  .books_reading-wrapper {
+    background-color: #202020;
+    .books_reading-title {
+      font-weight: 400;
+      font-size: 22px;
+      line-height: 144.4%;
+      color: #c9ac8c;
+      .books_reading-icon {
+        color: rgba(255, 255, 255, 0.3);
+      }
+    }
+    .books_reading-card {
+      display: flex;
+      align-items: center;
+      .books_reading-card-img {
+        width: 35px;
+        height: 45px;
+        margin-right: 20px;
+        img {
+          width: 120%;
+        }
+      }
+      .books_reading-text {
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 144.4%;
+        color: #ffffff;
+        margin-right: 10px;
+        p {
+          margin-top: 0;
+        }
+        span {
+          position: relative;
+          display: inline-block;
+          width: 150px;
+          height: 20px;
+          background: #ffffff;
+          border-radius: 5px;
+        }
+        span::before {
+          content: "";
+          display: inline-block;
+          position: absolute;
+          width: 100px;
+          height: 20px;
+          background-color: #a87f52;
+          border-radius: 5px;
+        }
+      }
+      .books_reading-percentage {
+        color: rgba(255, 255, 255, 0.6);
+        p {
+          text-align: end;
+          margin-top: 0;
+        }
+        button {
+          padding-top: 2px;
+          height: 20px;
+          font-weight: 400;
+          font-size: 10px;
+          line-height: 10px;
+          width: 90px;
+          color: #3c2710;
+          background: #c9ac8c;
+          border-radius: 5px;
+          border: none;
+          span {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            .books_reading-percentage-icon {
+              font-size: 16px;
+            }
+          }
         }
       }
     }
